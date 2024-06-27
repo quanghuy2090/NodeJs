5d980b6d23e06fc8087d5f0047f00cee72a0c653
quanghuy2090 <huynqph46213@fpt.edu.vn>
2024-06-27 13:30:32 +0700

    .

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ app.get("/", (req,res,next)=>{
     res.send("<h1>List Page</h1>")
 });
 
-app.get("/create", (req,res,next)=>{
+app.get("/create/:id", (req,res,next)=>{
+    console.log(req.params);
+    console.log(req.query);
     res.send("<h1>Create Page</h1>")
 });
 
