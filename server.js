ce3863d3388579caee590d5fc4c3ec8f5e254380
quanghuy2090 <huynqph46213@fpt.edu.vn>
2024-06-27 13:52:51 +0700

    .

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
-const express = require("express") 
+const express = require("express");
 const app = new express();
 const port = 3000;
 
-app.get("/", (req,res,next)=>{
-    res.send("<h1>List Page</h1>")
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.get("/list/:id", (req, res, next) => {
+  res.render("list", { idSp: req.params.id, nameSp: "phone" });
 });
 
-app.get("/create/:id", (req,res,next)=>{
-    console.log(req.params);
-    console.log(req.query);
-    res.send("<h1>Create Page</h1>")
+app.get("/create/:id", (req, res, next) => {
+  console.log(req.params);
+  console.log(req.query);
+  res.send("<h1>Create Page</h1>");
 });
 
-app.listen(port, ()=>{
-    console.log("Sv chay o port " + port);
-})
+app.listen(port, () => {
+  console.log("Sv chay o port " + port);
+});
