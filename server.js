78d30ee2c2fed486f3f4e2ec046f0a6e22e2f1cd
quanghuy2090 <huynqph46213@fpt.edu.vn>
2024-06-27 13:08:16 +0700

    .

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const express = require("express")
 const app = new express();
 const port = 3000;
 
+app.get("/", (req,res,next)=>{
+    res.send("<h1>List Page</h1>")
+});
+
+app.get("/create", (req,res,next)=>{
+    res.send("<h1>Create Page</h1>")
+});
+
 app.listen(port, ()=>{
     console.log("Sv chay o port " + port);
 })
